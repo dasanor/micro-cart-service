68e1c258cc8ee91ffb8849d524f0658439526f8f
Nicolás Cornaglia <ncornag@gmail.com>
2016-06-07 21:40:30 +0200

    Added ADDTOCART event

--- src/operations/addEntry/hooks/postSaveCart.js
+++ src/operations/addEntry/hooks/postSaveCart.js
@@ -1,9 +1,16 @@
 /**
  * Allows the customization of actions after the cart was stored
  */
-function addEntry(/* base */) {
+function addEntry(base) {
+  const cartsChannel = base.config.get('channels:carts');
   return (data /* cart, productId, quantity, warehouseId */) => {
     return new Promise((resolve /* , reject */) => {
+      base.events.send(cartsChannel, 'ADDTOCART', {
+        cart: data.cart.toObject({ virtuals: true }),
+        productId: data.productId,
+        quantity: data.quantity,
+        warehouseId: data.warehouseId
+      });
       resolve(data);
     });
   };
