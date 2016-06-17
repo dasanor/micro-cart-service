56e532f7793718d055ae9ed6e6bf5a49c6b271f2
Nicolás Cornaglia <ncornag@gmail.com>
2016-06-17 23:20:16 +0200

    Fixed Cart tests. Added taxes tests.

--- src/test/test.js
+++ src/test/test.js
@@ -36,13 +36,45 @@ if (!base.db.url.includes('test')) {
   process.exit(1);
 }
 
+function createTaxes() {
+  return server.inject({
+      method: 'POST',
+      url: '/services/cart/v1/tax',
+      headers: defaultHeaders,
+      payload: {
+        code: 'default-percentage',
+        class: 'default',
+        title: 'Tax 21%',
+        rate: 21,
+        isPercentage: true
+      }
+    })
+    .then(() => {
+      return server.inject({
+        method: 'POST',
+        url: '/services/cart/v1/tax',
+        headers: defaultHeaders,
+        payload: {
+          code: 'default-fixed',
+          class: 'default',
+          title: 'Tax 5',
+          rate: 5,
+          isPercentage: false
+        }
+      })
+    })
+}
+
 // Helper to clean the database
 function initDB(done) {
   connect(base.db.url, (err, db) => {
     databaseCleaner.clean(db, () => {
       console.log('[test] database cleaned');
-      db.close();
-      done();
+      createTaxes()
+        .then(() => {
+          db.close();
+          done();
+        })
     });
   });
 }
@@ -84,24 +116,57 @@ function mockStockReserveNoEnoughStock(entryRequest) {
     });
 }
 
+// Helper to mock a product data get
+function mockProductDataGet(entryRequest) {
+  nock('http://gateway')
+    .get(`/services/catalog/v1/product/${entryRequest.productId}`)
+    .reply(200, {
+      price: 1260,
+      salePrice: 1041.26,
+      taxCode: 'default-percentage',
+      isNetPrice: false,
+      categories: [],
+      id: entryRequest.productId
+    });
+}
+
+// Helper to mock a product tax data get
+function mockProductTaxDataGet(entryRequest) {
+  nock('http://gateway')
+    .get(`/services/catalog/v1/product?id=${entryRequest.productId}&fields=taxCode,categories,isNetPrice`)
+    .reply(200, {
+      page: { limit: 10, skip: 0 },
+      data: [
+        {
+          isNetPrice: false,
+          categories: [],
+          id: entryRequest.productId,
+          taxCode: 'default-percentage'
+        }
+      ]
+    });
+}
+
 // Helper to create carts
-function createCart(numEntries) {
+function createCart(numEntries, cartEntryRequest) {
   let cart;
   return server.inject({
       method: 'POST',
       url: '/services/cart/v1',
       headers: defaultHeaders
     })
-    .then(response => {
+    .then(cartResponse => {
       if (numEntries) {
-        const entryRequest = {
+        const entryRequest = cartEntryRequest || {
           productId: '0001',
           quantity: 10,
           warehouseId: '001'
         };
-        cart = response.result;
+        cart = cartResponse.result;
 
         const addEntry = function (cartId) {
+          mockProductDataGet(entryRequest);
+          mockProductTaxDataGet(entryRequest);
           mockStockReserveOk(entryRequest);
           return new Promise((resolve, reject) => {
             server.inject({
@@ -110,8 +175,17 @@ function createCart(numEntries) {
                 payload: entryRequest,
                 headers: defaultHeaders
               })
-              .then(response => resolve(response))
-              .catch(error => reject(error));
+              .then(entryResponse => {
+                if (entryResponse.result && entryResponse.result.statusCode) {
+                  console.error(entryResponse.result);
+                  return reject(entryResponse.result);
+                }
+                resolve(entryResponse);
+              })
+              .catch(error => {
+                console.error(error);
+                reject(error);
+              });
           });
         };
         const allAddEntries = Array.from(new Array(numEntries), () => addEntry(cart.id));
@@ -124,10 +198,19 @@ function createCart(numEntries) {
                 url: `/services/cart/v1/${cart.id}`,
                 headers: defaultHeaders
               })
-              .then(response => response.result);
+              .then(response => {
+                if (response.result && response.result.statusCode) {
+                  console.error(response.result);
+                }
+                return response.result;
+              })
+              .catch(error => {
+                console.error(error);
+                return error;
+              });
           });
       }
-      return response.result;
+      return cartResponse.result;
     });
 }
 
@@ -223,7 +306,6 @@ describe('Cart Entries', () => {
     initDB(done);
   });
 
-
   it('adds an entry to a non-existent cart', (done) => {
     const entryRequest = {
       productId: '0001',
@@ -263,6 +345,8 @@ describe('Cart Entries', () => {
     createCart()
       .then(cart => {
         mockStockReserveOk(entryRequest);
+        mockProductDataGet(entryRequest);
+        mockProductTaxDataGet(entryRequest);
         const options = {
           method: 'POST',
           url: `/services/cart/v1/${cart.id}/entry`,
@@ -303,9 +387,10 @@ describe('Cart Entries', () => {
   });
 
   it('adds an entry with a quantity > maxQuantityPerProduct', (done) => {
+    const maxQuantityPerProduct = base.config.get('hooks:preAddToCart:maxQuantityPerProduct');
     const entryRequest = {
       productId: '0001',
-      quantity: base.config.get('hooks:preAddToCart:maxQuantityPerProduct') + 1,
+      quantity: maxQuantityPerProduct + 1,
       warehouseId: '001'
     };
     createCart()
@@ -330,19 +415,20 @@ describe('Cart Entries', () => {
         const result = response.result;
         expect(result.statusCode).to.be.a.number().and.to.equal(406);
         expect(result.error).to.be.a.string().and.to.equal('Not Acceptable');
-        expect(result.message).to.be.a.string().and.to.startWith('Quantity in cart for this product must be less than or equal to');
+        expect(result.message).to.be.a.string().and.to.startWith(`Quantity in cart for this product must be less or equal than '${maxQuantityPerProduct}'`);
         done();
       })
       .catch((error) => done(error));
   });
 
   it('adds an entry with a full cart', (done) => {
+    const maxNumberOfEntries = base.config.get('hooks:preAddToCart:maxNumberOfEntries');
     const entryRequest = {
       productId: '0001',
       quantity: 10,
       warehouseId: '001'
     };
-    createCart(base.config.get('hooks:preAddToCart:maxNumberOfEntries') + 1)
+    createCart(maxNumberOfEntries, entryRequest)
       .then(cart => {
         const options = {
           method: 'POST',
@@ -360,12 +446,12 @@ describe('Cart Entries', () => {
         // {
         //   statusCode: 406,
         //   error: 'Not Acceptable',
-        //   message: 'Quantity in cart for this product must be less than or equal to ${maxQuantityPerProduct}'
+        //   message: 'Number of entries must be less or equal than ${maxQuantityPerProduct}'
         // }
         const result = response.result;
         expect(result.statusCode).to.be.a.number().and.to.equal(406);
         expect(result.error).to.be.a.string().and.to.equal('Not Acceptable');
-        expect(result.message).to.be.a.string().and.to.startWith('Number of entries must be less than or equal to');
+        expect(result.message).to.be.a.string().and.to.startWith(`Number of entries must be less or equal than '${maxNumberOfEntries}'`);
         done();
       })
       .catch((error) => done(error));
@@ -379,6 +465,7 @@ describe('Cart Entries', () => {
     };
     createCart()
       .then(cart => {
+        mockProductDataGet(entryRequest);
         mockStockReserveNoEnoughStock(entryRequest);
         const options = {
           method: 'POST',
@@ -386,7 +473,6 @@ describe('Cart Entries', () => {
           payload: entryRequest,
           headers: defaultHeaders
         };
-
         return server.inject(options);
       })
       .then(response => {
@@ -405,6 +491,60 @@ describe('Cart Entries', () => {
         done();
       })
       .catch((error) => done(error));
+  });
+
+});
+
+describe('Taxes', () => {
+  before((done) => {
+    initDB(done);
+  });
+  after((done) => {
+    initDB(done);
+  });
+
+  it('calculates gross percentage tax', (done) => {
+    const cart = {};
+    const item = { quantity: 1, price: 100 };
+    const product = { isNetPrice: false };
+    const taxCode = 'default-percentage';
+    const { beforeTax, tax, taxDetail } = base.taxesCalculationService.calculateItemTaxes(cart, item, product, taxCode);
+    expect(beforeTax).to.be.a.number().and.to.equal(100);
+    expect(tax).to.be.a.number().and.to.equal(21);
+    done();
+  });
+
+  it('calculates net percentage tax', (done) => {
+    const cart = {};
+    const item = { quantity: 1, price: 100 };
+    const product = { isNetPrice: true };
+    const taxCode = 'default-percentage';
+    const { beforeTax, tax, taxDetail } = base.taxesCalculationService.calculateItemTaxes(cart, item, product, taxCode);
+    expect(beforeTax).to.be.a.number().and.to.equal(79);
+    expect(tax).to.be.a.number().and.to.equal(21);
+    done();
+  });
+
+  it('calculates gross fixed tax', (done) => {
+    const cart = {};
+    const item = { quantity: 1, price: 100 };
+    const product = { isNetPrice: false };
+    const taxCode = 'default-fixed';
+    const { beforeTax, tax, taxDetail } = base.taxesCalculationService.calculateItemTaxes(cart, item, product, taxCode);
+    expect(beforeTax).to.be.a.number().and.to.equal(100);
+    expect(tax).to.be.a.number().and.to.equal(5);
+    done();
+  });
+
+  it('calculates net fixed tax', (done) => {
+    const cart = {};
+    const item = { quantity: 1, price: 100 };
+    const product = { isNetPrice: true };
+    const taxCode = 'default-fixed';
+    const { beforeTax, tax, taxDetail } = base.taxesCalculationService.calculateItemTaxes(cart, item, product, taxCode);
+    expect(beforeTax).to.be.a.number().and.to.equal(95);
+    expect(tax).to.be.a.number().and.to.equal(5);
+    done();
   });
 
 });
