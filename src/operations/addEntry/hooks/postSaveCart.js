bd8a75cab8657dda84e2c266c7d7f6856a1f0090
Nicolás Cornaglia <ncornag@gmail.com>
2016-07-07 22:04:47 +0200

    Use the bus service.

--- src/operations/addEntry/hooks/postSaveCart.js
+++ src/operations/addEntry/hooks/postSaveCart.js
@@ -2,9 +2,9 @@
  * Actions after the cart was stored
  */
 function factory(base) {
-  const cartsChannel = base.config.get('channels:carts');
+  const cartsChannel = base.config.get('bus:channels:carts:name');
   return (context, next) => {
-    base.events.send(cartsChannel, 'ADDTOCART', {
+    base.bus.publish(`${cartsChannel}.ADDTOCART`, {
       cart: context.cart.toObject({ virtuals: true }),
       addedEntries: context.addedEntries
     });
