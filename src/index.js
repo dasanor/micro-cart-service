4e19bad23b7a0367acc5231aa427ed3cd2e70d18
Nicolás Cornaglia <ncornag@gmail.com>
2016-09-21 23:31:33 +0200

    Use addOperation insted of the old add.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ require(base.config.get('models:cartModel'))(base);
 require(base.config.get('models:taxModel'))(base);
 
 // Add operations
-base.services.add(require('./operations/new')(base));
-base.services.add(require('./operations/get')(base));
-base.services.add(require('./operations/addEntry')(base));
-base.services.add(require('./operations/removeEntry')(base));
-base.services.add(require('./operations/taxes/createTax')(base));
-base.services.add(require('./operations/taxes/cartTaxes')(base));
+base.services.addOperation(require('./operations/new')(base));
+base.services.addOperation(require('./operations/get')(base));
+base.services.addOperation(require('./operations/addEntry')(base));
+base.services.addOperation(require('./operations/removeEntry')(base));
+base.services.addOperation(require('./operations/taxes/createTax')(base));
+base.services.addOperation(require('./operations/taxes/cartTaxes')(base));
 
 module.exports = base;
