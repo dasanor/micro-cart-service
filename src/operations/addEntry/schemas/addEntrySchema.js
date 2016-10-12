0a26304bb4c44dfd7a6a81738b9946e63219e3ab
Nicolás Cornaglia <ncornag@gmail.com>
2016-10-12 20:18:34 +0200

    Updated to microbase 0.8.0. Use amqp by default.

--- src/operations/addEntry/schemas/addEntrySchema.js
+++ src/operations/addEntry/schemas/addEntrySchema.js
@@ -1,35 +1,33 @@
 module.exports = {
-  payload: {
-    type: 'object',
-    properties: {
+  type: 'object',
+  properties: {
+    items: {
+      type: 'array',
+      minItems: 1,
       items: {
-        type: 'array',
-        minItems: 1,
-        items: {
-          type: 'object',
-          properties: {
-            productId: {
-              type: 'string'
-            },
-            quantity: {
-              type: 'integer',
-              minimum: 1
-            },
-            warehouseId: {
-              type: 'string'
-            }
+        type: 'object',
+        properties: {
+          productId: {
+            type: 'string'
           },
-          required: [
-            'productId', 'quantity'
-          ],
-          additionalProperties: true
-        }
+          quantity: {
+            type: 'integer',
+            minimum: 1
+          },
+          warehouseId: {
+            type: 'string'
+          }
+        },
+        required: [
+          'productId', 'quantity'
+        ],
+        additionalProperties: true
       }
-    },
-    required: [
-      'items'
-    ],
-    additionalProperties: true
-  }
+    }
+  },
+  required: [
+    'items'
+  ],
+  additionalProperties: true
 };
 
